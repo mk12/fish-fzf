f626f33e25792fa2aa8c68fa2c07f52162c41460
Mitchell Kember <mk12360@gmail.com>
2022-10-03 12:13:43 -0700

    Add workaround for 6942
    See https://github.com/fish-shell/fish-shell/issues/6942#issuecomment-984737309

--- conf.d/fzf.fish
+++ conf.d/fzf.fish
@@ -4,7 +4,13 @@ if not status is-interactive
     exit
 end
 
-bind \co "__fzf_insert file"
-bind \cq "__fzf_insert directory"
-bind \ez "__fzf_insert z"
-bind \cr "__fzf_history"
+# Workaround for:
+# https://github.com/fish-shell/fish-shell/issues/6942#issuecomment-984737309
+function __fzf_try_disable_focus
+    functions -q __fish_disable_focus && __fish_disable_focus
+end
+
+bind \co __fzf_try_disable_focus "__fzf_insert file"
+bind \cq __fzf_try_disable_focus "__fzf_insert directory"
+bind \ez __fzf_try_disable_focus "__fzf_insert z"
+bind \cr __fzf_try_disable_focus "__fzf_history"
